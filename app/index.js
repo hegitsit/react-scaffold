c18aaefb5f1fa4227ed9921b95847d01ce0a5ec2
creationator <morton.derrick@gmail.com>
2020-04-20 02:44:14 -0400

    Created react starter app

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Yuuuurrrrrr</h1>
+      </Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
